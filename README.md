ff6ac4cf35cc1689de6fb534ddacdae9a391688e
Benjamin Bouvier <public@benj.me>
2021-09-09 19:40:29 +0200

    Commit initial.

---
+++ README.md
@@ -0,0 +1,28 @@
+# rouille
+
+Aren't you *le tired* from writing Rust programs in English? Do you like saying
+"merde" a lot? Would you like to try something different, in an exotic
+language?
+
+**rouille** (French for *Rust*) is here to save the day, and allows one to
+write Rust programs in French. See the [examples](./examples/src/main.rs) for
+more details about what can be done with it.
+
+## contributions
+
+First of all, *merci beaucoup* for considering participating to this joke, the
+French government will thank you later! Feel free to throw in a few identifiers
+here and there, and open a pull-request against the `principale` (French for
+`main`) branch.
+
+## but why would you do zat
+
+- horsin around
+- making a bit of fun of programming languages using French keywords
+- winking at [Marcel](https://github.com/brouberol/marcel)
+- playing with raw proc macros
+- c'est chic
+
+## license
+
+[WTFPL](http://www.wtfpl.net/).
---
+++ examples/src/main.rs
@@ -0,0 +1,69 @@
+rouille::rouille! {
+    utilisons std::collections::Dictionnaire comme Dico;
+
+    interface CléValeur {
+        fonction écrire(&soi, clé: Chaine, valeur: Chaine);
+        fonction lire(&soi, clé: Chaine) -> PeutÊtre<&Chaine>;
+    }
+
+    statique mutable DICTIONNAIRE: PeutÊtre<Dico<Chaine, Chaine>> = Rien;
+
+    structure Concrète;
+
+    implémentation CléValeur pour Concrète {
+        fonction écrire(&soi, clé: Chaine, valeur: Chaine) {
+            soit dico = dangereux {
+                DICTIONNAIRE.prendre_ou_insérer_avec(Défault::défault)
+            };
+            dico.insérer(clé, valeur);
+        }
+        fonction lire(&soi, clé: Chaine) -> PeutÊtre<&Chaine> {
+            soit dico = dangereux {
+                DICTIONNAIRE.prendre_ou_insérer_avec(Défault::défault)
+            };
+            dico.lire(&clé)
+        }
+    }
+
+    public(cagette) fonction peut_etre(i: u32) -> PeutÊtre<Résultat<u32, Chaine>> {
+        si i % 2 == 1 {
+            si i == 42 {
+                Quelque(Arf(Chaine::depuis("merde")))
+            } sinon {
+                Quelque(Bien(33))
+            }
+        } sinon {
+            Rien
+        }
+    }
+
+    asynchrone fonction exemple() {
+    }
+
+    asynchrone fonction exemple2() {
+        exemple().attend;
+    }
+
+    fonction principale() {
+        soit mutable x = 31;
+
+        correspond x {
+            42 => {
+                affiche!("omelette du fromage")
+            }
+            _ => affiche!("voila")
+        }
+
+        pour i de 0..10 {
+            soit val = boucle {
+                arrête i;
+            };
+            tant que x < val {
+                x += 1;
+            }
+            x = si soit Quelque(resultat) = peut_etre(i) {
+                resultat.déballer()
+            } else { 12 };
+        }
+    }
+}
